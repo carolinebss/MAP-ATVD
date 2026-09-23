5900e21d46322fb4517bae9cbe31edbf26b9cc3a
Caroline Barbosa <carolinebsbarbosa26@gmail.com>
2026-09-22 21:08:49 -0300

    3. Criar a classe Context

--- Pedido.java
+++ Pedido.java
@@ -6,11 +6,9 @@ public class Pedido {
         this.peso = peso;
         this.estrategiaFrete = estrategiaFrete;
     }
-
     public void setEstrategiaFrete(FreteStrategy estrategiaFrete) {   //responsável por permitir a troca de estratégia em tempo de execução
         this.estrategiaFrete = estrategiaFrete;
     }
-
     public double calcularFrete() {
         if (estrategiaFrete == null) {
             throw new IllegalStateException("Nenhuma estratégia de frete definida");
